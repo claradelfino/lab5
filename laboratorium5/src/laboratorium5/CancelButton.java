919b73ab27e3e6d275514c9e7b9b0c9de989ca51
Nikolaos10 <126576202+Nikolaos10@users.noreply.github.com>
2023-03-06 16:39:54 +0100

    Update CancelButton.java

--- laboratorium5/src/laboratorium5/CancelButton.java
+++ laboratorium5/src/laboratorium5/CancelButton.java
@@ -1,28 +1,48 @@
 package laboratorium5;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 
 @SuppressWarnings("serial")
 public class CancelButton extends CalculatorButton {
 	private Situation sit;
-	
 
 	public CancelButton(String buttonText, Situation sit) {
 		super(buttonText, sit);
-		this.setText(buttonText);		// TODO Auto-generated constructor stub}
+		this.setText(buttonText); // TODO Auto-generated constructor stub}
 	}
-		// TODO Auto-generated constructor stub
-		
-		
+	// TODO Auto-generated constructor stub
+
 	@Override
-	public void transition()  {
-//		switch
+	public void transition() {
+		switch (this.sit.state) {
+		case HasResult:
+			this.sit.state = State.Input1;
+			this.sit.setDispay();
+			break;
+		case Input1:
+			this.sit.setDispay();
+			break;
+		case Input2:
+			this.sit.state = State.Input1;
+			this.sit.setDispay();
+			break;
+		case OpReady:
+			this.sit.state = State.Input1;
+			this.sit.setDispay();
+
+			super.setColor(Color.LIGHT_GRAY);
+			break;
+		default:
+			break;
+
+		}
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
-
